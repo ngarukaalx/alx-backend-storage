5710c337002a760d665dd4fec4488847b5f1f2d5
ngarukaalx <hiramkabuagi@gmail.com>
2024-03-22 16:11:25 +0000

    fix trigger

--- 0x00-MySQL_Advanced/5-valid_email.sql
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -1,16 +1,17 @@
--- trigger that resets attibute valid_email only when the email has been changed
+-- trigger that resets attibute valid_email only when
+-- the email has been changed
 DELIMITER $$
 CREATE
 TRIGGER update_valid_email
 BEFORE UPDATE ON users
 FOR EACH ROW
-	BEGIN
-		IF NEW.email <> OLD.email THEN
-			IF OLD.valid_email = 0 THEN
-				SET NEW.valid_email = 1;
-			ELSE
-				SET NEW.valid_email = 0;
-			END IF;
+BEGIN
+	IF NEW.email <> OLD.email THEN
+		IF OLD.valid_email = 0 THEN
+			SET NEW.valid_email = 1;
+		ELSE
+			SET NEW.valid_email = 0;
 		END IF;
-	END;
-	$$
+	END IF;
+END;
+$$
