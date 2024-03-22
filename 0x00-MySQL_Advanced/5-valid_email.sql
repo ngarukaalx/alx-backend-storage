e5e3dcbda150118a2b3c6465c8757bf680e84069
ngarukaalx <hiramkabuagi@gmail.com>
2024-03-22 17:29:51 +0000

    trigger before

--- 0x00-MySQL_Advanced/5-valid_email.sql
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -7,7 +7,11 @@ BEFORE UPDATE ON users
 FOR EACH ROW
 BEGIN
 	IF NEW.email <> OLD.email THEN
-	SET NEW.valid_email = 0;
+		IF NEW.valid_email = 0 THEN
+			SET NEW.valid_email = 1;
+		ELSE
+			SET NEW.valid_email = 0;
+		END IF;
 	END IF;
 END;
 $$
