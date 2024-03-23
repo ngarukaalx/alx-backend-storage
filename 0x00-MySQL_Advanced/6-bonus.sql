fff2eb35b6791e96bf4802299a98ee5b455e7b03
ngarukaalx <hiramkabuagi@gmail.com>
2024-03-23 07:48:51 +0000

    procedure update values

--- 0x00-MySQL_Advanced/6-bonus.sql
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -1,17 +1,12 @@
 -- creates procedure AddBonus that adds new correction for a student
 DELIMITER $$
-CREATE PROCEDURE AddBonus(IN user_id INT, IN project_name VARCHAR(30), IN score INT)
+CREATE PROCEDURE AddBonus(IN userid INT, IN projectname VARCHAR(30), IN pscore FLOAT)
 BEGIN
 	DECLARE available_project INT;
-	DECLARE projectId INT;
-	SELECT COUNT(*) INTO available_project  FROM projects WHERE name = project_name;
-	IF available_project > 0 THEN
-		SELECT id INTO projectId FROM projects WHERE name = project_name;
-		UPDATE corrections SET score = score WHERE user_id = user_id AND project_id = projectId;
-	ELSE
-		INSERT INTO projects (name) VALUES(project_name);
-		SET @project_id = LAST_INSERT_ID();
-		INSERT INTO corrections (user_id, project_id, score) VALUES(user_id, @project_id, score);
+	SELECT COUNT(*) INTO available_project  FROM projects WHERE name = projectname;
+	IF available_project = 0 THEN
+		INSERT INTO projects (name) VALUES(projectname);
 	END IF;
+	INSERT INTO corrections (user_id, project_id, score) VALUES(userid, (SELECT id FROM projects WHERE name = projectname), pscore);
 END
 $$
