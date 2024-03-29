c13398551cf480b105e7608d3877e9a8d6aad5e6
ngarukaalx <hiramkabuagi@gmail.com>
2024-03-29 23:29:42 +0000

    replay

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -24,6 +24,26 @@ def count_calls(method: Callable) -> Callable:
         return method(self, *args, **kwargs)
     return wrapper
 
+
+def call_history(method: Callable) -> Callable:
+    """decoratorn func"""
+    @functools.wraps(method)
+    def wrapper(self, *args, **kwargs):
+        """wapper func"""
+        key_inputs = method.__qualname__ + ':inputs'
+        # store the method arguments to a list
+        inputs = str(args)
+        self._redis.rpush(key_inputs, inputs)
+
+        key_outputs = method.__qualname__ + ':outputs'
+        result = method(self, *args, **kwargs)
+        # store the output to a list
+        self._redis.rpush(key_outputs, result)
+        return result
+
+    return wrapper
+
+
 class Cache():
     """class to perform some redis operation"""
     def __init__(self):
@@ -33,8 +53,9 @@ class Cache():
 
         # flush the instance
         self._redis.flushdb()
-    
+
     @count_calls
+    @call_history
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """Generate a random key and store input data in redis using
         random key and return the key
@@ -42,18 +63,21 @@ class Cache():
         key = str(uuid.uuid4())
         self._redis.mset({key: data})
         return key
-    
+
     @count_calls
+    @call_history
     def get_int(self, key: str) -> int:
         """convert data in bytes to int"""
         return self.get(key, fn=int)
-    
+
     @count_calls
+    @call_history
     def get_str(self, key: str) -> str:
         """converts x to str"""
         return self.get(key, fn=lambda x: x.decode('utf-8'))
 
     @count_calls
+    @call_history
     def get(self, key: str, fn: Callable = None) -> Any:
         """get method"""
         result = self._redis.get(key)
@@ -64,3 +88,21 @@ class Cache():
         if fn is not None:
             result = fn(result)
         return result
+
+    def replay(self, arg):
+        """implements the replay to display history"""
+
+        class_name = arg.__self__.__class__.__name__
+        method_name = arg.__name__
+        arg_expression = f"{class_name}.{method_name}"
+
+        key_inputs = arg_expression + ':inputs'
+        key_outputs = arg_expression + ':outputs'
+        outputs = self._redis.lrange(key_outputs, 0, -1)
+        inputs = self._redis.lrange(key_inputs, 0, -1)
+        print("{} was called {} times:".format(
+            arg_expression, self._redis.llen(key_inputs))
+            )
+        for inp, out in zip(inputs, outputs):
+            print(f"{arg_expression}(*{inp.decode('utf-8')}) "
+                  f"-> {out.decode('utf-8')}")
--- 0x02-redis_basic/main.py
+++ 0x02-redis_basic/main.py
@@ -8,14 +8,7 @@ Cache = __import__('exercise').Cache
 
 cache = Cache()
 
-TEST_CASES = {
-    b"foo": None,
-    123: int,
-    "bar": lambda d: d.decode("utf-8")
-}
-
-for value, fn in TEST_CASES.items():
-    key = cache.store(value)
-    result = cache.get(key, fn=fn)
-    print("result is: {} and value is: {}".format(result, value))
-    assert cache.get(key, fn=fn) == value
+cache.store("foo")
+cache.store("bar")
+cache.store(42)
+cache.replay(cache.store)
