52b912156ae8bbc1264454c6c28446f862832ac6
ngarukaalx <hiramkabuagi@gmail.com>
2024-03-29 23:51:09 +0000

    request

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,19 +1,19 @@
 #!/usr/bin/env python3
 """This module contains func get_page"""
 
-
+import requests
 import redis
 
 
 r = redis.Redis()
-get_page(url: str) -> str:
+def get_page(url: str) -> str:
     """track how many times a particular URL was accessed"""
     url_key = f"count:{url}"
     r.incr(url_key)
 
     response = request.get(url)
-    htm_cont = response.text
+    htm_content = response.text
 
     key_cached = f"cache:{url}"
-    r.setex(key_cached, 10, htm_cont)
-    return htm_cont
+    r.setex(key_cached, 10, htm_content)
+    return htm_content
