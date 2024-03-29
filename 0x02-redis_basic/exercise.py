3385bae114c8c4a84b5e26179fe5c3e1ea157fea
ngarukaalx <hiramkabuagi@gmail.com>
2024-03-29 11:31:51 +0000

    port/host_checking

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -13,7 +13,7 @@ class Cache():
     def __init__(self):
         """store an instance of the Redis client as a private variable
         """
-        self._redis = redis.Redis()
+        self._redis = redis.Redis(host='127.0.0.1', port=6379)
 
         # flush the instance
         self._redis.flushdb()
